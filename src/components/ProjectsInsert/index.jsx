ea08acacc63631a11e0cc80967388064b99162b0
Zanetti Jordan <jzanetti1@outlook.fr>
2023-05-19 20:29:20 +0200

    Add ProjectInsert component to display projects via JSON data

--- src/components/ProjectsInsert/index.jsx
+++ src/components/ProjectsInsert/index.jsx
@@ -3,9 +3,16 @@ import React from 'react';
 const ProjectsInsert = ({ projet }) => {
   return (
     <li>
-      <div className="img-container"></div>
+      <div className="img-container">
+        <img src={projet.imageUrl} alt={projet.name} />
+      </div>
       <div className="infos-container">
         <h4>{projet.name}</h4>
+        <h5>{projet.category}</h5>
+        <p>{projet.description}</p>
+        <a target="_blank" rel="noreferrer" href={projet.url}>
+          Visiter le site
+        </a>
       </div>
     </li>
   );
