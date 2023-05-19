cba93445a784f11a9962a3bd38faf1ecaeb7382b
Zanetti Jordan <jzanetti1@outlook.fr>
2023-05-19 14:57:51 +0200

    Add styles header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,22 @@ const Header = () => {
   return (
     <header>
       <div className="container">
-        <Navigation />
+        <div className="row">
+          <div className="col-lg-12">
+            <Navigation />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-6">
+            <h1>
+              Zanetti Jordan <br />
+              <span>Développeur web front-end</span>
+            </h1>
+            <a className="btn-style" href="#contact">
+              Contactez moi
+            </a>
+          </div>
+        </div>
       </div>
     </header>
   );
