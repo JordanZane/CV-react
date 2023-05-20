b3de37de4269048a19be24148ce2ece51623c2d1
Zanetti Jordan <jzanetti1@outlook.fr>
2023-05-20 18:00:21 +0200

    Add animations on scrolls for skills inserts + h2 titles

--- src/components/ScrollAnim/scrollanim.js
+++ src/components/ScrollAnim/scrollanim.js
@@ -3,9 +3,7 @@ import React, { useEffect } from 'react';
 const ScrollAnim = () => {
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
-      console.log('test');
       entries.forEach((entry) => {
-        console.log(entry);
         if (entry.isIntersecting) {
           entry.target.classList.add('show');
         } else {
