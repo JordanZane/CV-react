89690cab1b8f5ff8c0dee68b5fb1df2de20729d8
Zanetti Jordan <jzanetti1@outlook.fr>
2023-05-20 14:15:03 +0200

    Add Contact component + styles

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,32 +6,39 @@ const Footer = () => {
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
-            <h2>Contact</h2>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-lg-4">
-            <div className="contact-content">
-              <i className="fa-solid fa-phone"></i>
-              <a href="tel:0666179121">06.66.17.91.21</a>
-            </div>
-          </div>
-          <div className="col-lg-4">
-            <div className="contact-content">
-              <i className="fa-sharp fa-solid fa-envelope"></i>{' '}
-              <a href="mailto:jzanetti1@outlook.fr">jzanetti1@outlook.fr</a>
-            </div>
-          </div>
-          <div className="col-lg-4">
-            <div className="contact-content">
-              <i className="fa-brands fa-linkedin-in"></i>{' '}
-              <a
-                href="https://www.linkedin.com/in/jordan-zanetti-044ab2174/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Profil LinkedIn
-              </a>
+            <div className="contact-container">
+              <h2>Contact</h2>
+              <div className="row">
+                <div className="col-lg-4">
+                  <div className="contact-content">
+                    <a href="tel:0666179121">
+                      {' '}
+                      <i className="fa-solid fa-phone"></i>
+                      06.66.17.91.21
+                    </a>
+                  </div>
+                </div>
+                <div className="col-lg-4">
+                  <div className="contact-content">
+                    <a href="mailto:jzanetti1@outlook.fr">
+                      <i className="fa-sharp fa-solid fa-envelope"></i>{' '}
+                      jzanetti1@outlook.fr
+                    </a>
+                  </div>
+                </div>
+                <div className="col-lg-4">
+                  <div className="contact-content">
+                    <a
+                      href="https://www.linkedin.com/in/jordan-zanetti-044ab2174/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <i className="fa-brands fa-linkedin-in"></i> Profil
+                      LinkedIn
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
